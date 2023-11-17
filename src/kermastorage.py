3437932656a2d0805b23db39ce696c43c6b73d41
rsaSantos <rs.elsaint@gmail.com>
2023-11-17 14:10:33 +0100

    db optimized

--- src/kermastorage.py
+++ src/kermastorage.py
@@ -6,17 +6,22 @@ import json
 import objects
 import constants as const
 
+BLOCK = "block"
+TRANSACTION = "transaction"
+
 TABLE_BLOCKS = "blocks"
 TABLE_TRANSACTIONS = "transactions"
 
-GET_BLOCK_QUERY = "SELECT block_data FROM " + TABLE_BLOCKS + " WHERE block_id=?"
-GET_UTXO_QUERY = "SELECT utxo_set FROM " + TABLE_BLOCKS + " WHERE block_id=?"
-GET_TRANSACTION_QUERY = "SELECT transaction_data FROM " + TABLE_TRANSACTIONS + " WHERE transaction_id=?"
+GET_BLOCK_QUERY = "SELECT * FROM " + TABLE_BLOCKS + " WHERE block_id=?"
+CHECK_BLOCK_EXISTS_QUERY = "SELECT COUNT(*) FROM " + TABLE_BLOCKS + " WHERE block_id=?"
+
+GET_TRANSACTION_QUERY = "SELECT * FROM " + TABLE_TRANSACTIONS + " WHERE transaction_id=?"
+CHECK_TRANSACTION_EXISTS_QUERY = "SELECT COUNT(*) FROM " + TABLE_TRANSACTIONS + " WHERE transaction_id=?"
 
 def get_object_table_name(obj_type):
-    if obj_type == "block":
+    if obj_type == BLOCK:
         return TABLE_BLOCKS
-    if obj_type == "transaction":
+    if obj_type == BLOCK:
         return TABLE_TRANSACTIONS
     return None
 
@@ -25,7 +30,23 @@ def get_connection():
 
 # Reduce the usage of this method if we want to get the object anyway!
 def check_objectid_exists(obj_id):
-    return get_object(obj_id) is not None
+    global TABLE_BLOCKS, TABLE_TRANSACTIONS
+
+    con = get_connection()
+    try:
+        cur = con.cursor()
+        cur.execute(CHECK_TRANSACTION_EXISTS_QUERY, (obj_id,))
+        row = cur.fetchone()
+        if row[0] == 0:
+            cur.execute(CHECK_BLOCK_EXISTS_QUERY, (obj_id,))
+            row = cur.fetchone()
+        
+        return row[0] > 0    
+    except Exception as e:
+        print("Error checking object id: " + str(e))
+        return False
+    finally:
+        con.close()
 
 # Saving an object in the database. The object can be either a block or a transaction
 def save_object(obj_id, obj_dict, utxo_set = None):
@@ -41,12 +62,14 @@ def save_object(obj_id, obj_dict, utxo_set = None):
     con = get_connection()
     try:        
         cur = con.cursor()
-        if(table_name == "transaction" or obj_dict["previd"] is None):
+        if table_name == TABLE_BLOCKS:
             cur.execute("INSERT INTO " + table_name + " VALUES (?,?)", (obj_id, canonicalize(obj_dict)))
-        else:
+        elif table_name == TABLE_TRANSACTIONS:
             cur.execute("INSERT INTO " + table_name + " VALUES (?,?,?)", (obj_id, canonicalize(obj_dict), utxo_set))
+
         con.commit()
         return True
+
     except Exception as e:
         con.rollback()
         print("Error saving object: " + str(e))
@@ -54,18 +77,65 @@ def save_object(obj_id, obj_dict, utxo_set = None):
     finally:
         con.close()
 
-def get_block(block_id):
-    return get_object(block_id, "block")
+# Returns all the columns of the block table (without block_id)
+def get_block_full(block_id):
+    block_row = get_object(block_id, BLOCK)
 
-def get_transaction(transaction_id):
-    return get_object(transaction_id, "transaction")
+
+    return get_object(block_id, BLOCK) # (block_id, block_data, utxo_set)
+
+# Returns the block data
+def get_block_data(block_id):
+    block_row = get_object(block_id, BLOCK)
+    if block_row is None:
+        return None
+    else:
+        return block_row[1] # (block_id, block_data, utxo_set)
+
+# Returns the utxo set
+def get_utxo_set(block_id):
+    block_row = get_object(block_id, BLOCK)
+    if block_row is None:
+        return None
+    else:
+        return block_row[2] # (block_id, block_data, utxo_set)
+
+def get_transaction_data(transaction_id):
+    transaction_row = get_object(transaction_id, TRANSACTION)
+    if transaction_row is None:
+        return None
+    else:
+        return transaction_row[1] # (tx_id, tx_data)
+
+def handle_row(row, return_only_data):
+    # If the row is empty, return None
+    if row is None:
+        return None
+    if return_only_data:
+        return json.loads(row[1])
+    
+    # Iterate tuple and convert respective JSON strings to python objects
+    ret = []
+    for i in range(len(row)):
+        decoded = row[i] if type(row[i]) != bytes else row[i].decode("utf-8")
+
+        if decoded is None:
+            ret.append(None)
+        elif decoded.startswith("{"):
+            ret.append(json.loads(decoded))
+        else:
+            ret.append(decoded)
+
+    return ret
 
 # Only use this method when only the object id is known.
-def get_object(obj_id, obj_type=None, utxo_needed=False):
+def get_object(obj_id, obj_type=None):
     global TABLE_BLOCKS, TABLE_TRANSACTIONS
 
     # Get table name based on object type.
     # If the object type is not specified, use the transaction table by default.
+    check_all_tables = obj_type is None
+    return_only_data = check_all_tables # If we queried without specifying the object type, we only want the data
     table_name = get_object_table_name(obj_type)
     if table_name is None:
         table_name = TABLE_TRANSACTIONS
@@ -75,22 +145,19 @@ def get_object(obj_id, obj_type=None, utxo_needed=False):
         cur = con.cursor()
         if table_name == TABLE_TRANSACTIONS:
             cur.execute(GET_TRANSACTION_QUERY, (obj_id,))
-        elif table_name == TABLE_BLOCKS and not utxo_needed:
+        elif table_name == TABLE_BLOCKS:
             cur.execute(GET_BLOCK_QUERY, (obj_id,))
-        elif table_name == TABLE_BLOCKS and utxo_needed:
-            cur.execute(GET_UTXO_QUERY, (obj_id,))
         else:
             raise Exception("Error in finding database table. Unknown object type: " + obj_type)
 
         row = cur.fetchone()
 
-        # We got no results, let's search the block table only if we were searching the transaction table
-        # If we already searched the block table it means that was the intended table and we should return None
-        if row is None and table_name == TABLE_TRANSACTIONS:
+        # We got no results, let's search the block table if we didn't already...
+        if check_all_tables and row is None:
             cur.execute(GET_BLOCK_QUERY, (obj_id,))
             row = cur.fetchone()
 
-        return None if row is None else json.loads(row[0])
+        return None if row is None else handle_row(row, return_only_data)
     except Exception as e:
         print("Error getting object: " + str(e))
         return None
@@ -108,16 +175,11 @@ def create_db():
         cur = con.cursor()
 
         # Build database
-        #
-        # For now its just one table with the following columns:
-        # - object_id (TEXT)
-        # - object (TEXT -> JSON)
-        #
         cur.execute("CREATE TABLE " + TABLE_BLOCKS + " (block_id TEXT PRIMARY KEY, block_data BLOB, utxo_set BLOB)")
         cur.execute("CREATE TABLE " + TABLE_TRANSACTIONS + " (transaction_id TEXT PRIMARY KEY, transaction_data BLOB)")
         # Preload genesis block
         genesis_block = canonicalize(const.GENESIS_BLOCK)
-        genesis_block_row = (const.GENESIS_BLOCK_ID, genesis_block, '[]'.encode('utf-8'))
+        genesis_block_row = (const.GENESIS_BLOCK_ID, genesis_block, None)
         cur.execute("INSERT INTO " + TABLE_BLOCKS + " VALUES (?,?,?)", genesis_block_row)
         con.commit()
 
